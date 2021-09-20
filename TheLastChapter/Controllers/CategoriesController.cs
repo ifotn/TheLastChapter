661facff8cae4317dc23a3c86486bc19174d6177
ifotn <rich@infrontofthenet.com>
2021-09-20 15:24:31 -0400

    l2/3 - mock Category model w/IEnumerable

--- TheLastChapter/Controllers/CategoriesController.cs
+++ TheLastChapter/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheLastChapter.Models;
 
 namespace TheLastChapter.Controllers
 {
@@ -10,7 +11,16 @@ namespace TheLastChapter.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // use the Category model to generate 10 categories in memory for display in the view
+            var categories = new List<Category>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // must now pass the strongly-typed data to the view
+            return View(categories);
         }
 
         public IActionResult Browse(string category)
