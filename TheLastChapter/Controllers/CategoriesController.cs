7b4430e5903f2967d9d1221fb9d3d6b147015232
ifotn <rich@infrontofthenet.com>
2021-09-20 14:50:53 -0400

    l2/2 - category model

---
+++ TheLastChapter/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLastChapter.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Browse(string category)
+        {
+            // if category empty, redirect to index so user can first pick a category
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // display the selected category using the ViewBag object
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
---
+++ TheLastChapter/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLastChapter.Models
+{
+    public class Category
+    {
+        // pk fields should always be called either {Model}Id or just Id
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
