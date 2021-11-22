627bbe1ff3d5fe953ad650481a6b10b6eeb38b1d
ifotn <rich@infrontofthenet.com>
2021-11-22 15:20:13 -0500

    l10/3 - index tests complete w/mock data

--- TheLastChapterTests/BooksControllerTests.cs
+++ TheLastChapterTests/BooksControllerTests.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TheLastChapter.Controllers;
 using TheLastChapter.Data;
+using TheLastChapter.Models;
 
 namespace TheLastChapterTests
 {
@@ -17,6 +18,7 @@ namespace TheLastChapterTests
         // class level vars for db
         private ApplicationDbContext _context;  // in-memory db connection object
         private BooksController controller;
+        List<Book> books = new List<Book>();
 
          // this runs automatically before each test in this class
         [TestInitialize]
@@ -29,7 +31,40 @@ namespace TheLastChapterTests
             _context = new ApplicationDbContext(options);
 
             // create mock data for testing
+            var Category = new Category
+            {
+                CategoryId = 1000, Name = "My Test Category"
+            };
 
+            books.Add(new Book
+            {
+                BookId = 642, Title = "A Book", Author = "Some Person", Price = 9.99, CategoryId = 1000,
+                Category = Category
+            });
+            books.Add(new Book
+            {
+                BookId = 529,
+                Title = "My Book",
+                Author = "Some Writer",
+                Price = 9.99,
+                CategoryId = 1000,
+                Category = Category
+            });
+            books.Add(new Book
+            {
+                BookId = 753,
+                Title = "Great Book",
+                Author = "Famous Writer",
+                Price = 9.99,
+                CategoryId = 1000,
+                Category = Category
+            });
+
+            foreach (var book in books)
+            {
+                _context.Books.Add(book);
+            }
+            _context.SaveChanges();
 
             // arrange: create controller for all tests
             controller = new BooksController(_context);
@@ -45,6 +80,17 @@ namespace TheLastChapterTests
 
             // assert
             Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public void IndexLoadsBooks()
+        {
+            // act
+            var result = (ViewResult)controller.Index().Result;
+            List<Book> model = (List<Book>)result.Model;
+
+            // assert
+            CollectionAssert.AreEqual(books.OrderBy(b => b.Author).ThenBy(b => b.Title).ToList(), model);
         }
     } 
 }
